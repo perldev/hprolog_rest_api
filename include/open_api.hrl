8f1865e10c9a01472466c14093a62d56df1a84cd
Bogdan Tchaicka <perldev@mail.ru>
2013-05-17 16:54:03 +0300

    logins cacheing

--- include/open_api.hrl
+++ include/open_api.hrl
@@ -1,5 +1,21 @@
--define('API_LOG'(Str, Pars ), io:format(Str, Pars) ).
--define(TEMP_SHELL_AIM, temp_aim).
--define(ERWS_LINK, erws_link_trace).
+-define('API_LOG'(Str, Pars ), log4erl:debug(Str, Pars) ).
+-define('AUTH_LOG'(Str, Pars ), log4erl:debug(Str, Pars) ).
 
--define(RULES, rules_inner).
+-define(WORK_PORT,8313).
+-define(COUNT_LISTENERS,10).
+
+-define(LOG_START,   application:start(log4erl), log4erl:conf(?LOG_CONF_FILE) ).
+-define(LOG_CONF_FILE, "log.conf").
+-define(REGISTERED_FILE, "registered.ets" ).
+-define(REGISTERED_NAMESPACE, "namespaces.ets" ).
+-define(CACHE_CONNECTION, 10000 ).%miliseconds
+
+
+
+-define(AUTH_LIST,
+		[ 
+	       { { {127,0,0,1},  "p24"}, yes  },
+	       { { {10,1,214,15},  "p24"}, yes  },
+	       { { {10,1,214,15},  "p24error"}, yes  }
+	       ]
+).
--- new.sh
+++ new.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
      
-curl -d "params=[{\"name\":\"X1\"}, {\"name\":\"X2\"},{\"name\":\"X3\"}, {\"name\":\"X4\"}, {\"name\":\"X5\"},{\"name\":\"X6\"},{\"name\":\"X7\"}, {\"name\":\"X8\"},{\"name\":\"X9\"}, {\"name\":\"X10\"}, {\"name\":\"X11\"},{\"name\":\"X12\"},{\"name\":\"X13\"},{\"name\":\"X14\"}  ]"   http://127.0.0.1:8313/prolog/create/get_fraud
+curl -d "params=[\"P24A794745162795672\",\"DNHE\",\"1.0\",\"UAH\",\"%D0%9A%D1%83%D0%BB%D0%B8%D0%BA+%D0%9D%D0%B0%D1%82%D0%B0%D0%BB%D0%B8%D1%8F+%D0%98%D0%B2%D0%B0%D0%BD%D0%BE%D0%B2%D0%BD%D0%B0\",\"4405885817280529\",\"0714\",\"%2B380506494896\",\"%D0%94%D0%9E%D0%94+%D0%92%D0%90%D0%A2+%22%D0%A0%D0%B0%D0%B9%D1%84%D1%84%D0%B0%D0%B9%D0%B7%D0%B5%D0%BD+%D0%91%D0%B0%D0%BD%D0%BA+%22%D0%90%D0%B2%D0%B0%D0%BB%D1%8C%22\",\"26003205000100\",\"undefined\",\"305653\",\"25543196\",\"%D0%BF%D0%BB%D0%B0%D1%82%D0%B0+%D0%B7%D0%B0+%D0%BC%D0%BE%D0%B4%D0%B5%D0%BC+%D0%BF%D0%BE+%D0%B0%D0%BA%D1%86%D0%B8%D0%B8+%D0%A1%D0%B0%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA+%D0%9C.%D0%9C.+%D0%BB%2F%D1%81+181014810+%D1%82%D0%B5%D0%BB.+51700+%D0%9F%D0%BB-%D1%89%D0%B8%D0%BA%3A+%D0%9A%D0%A3%D0%9B%D0%98%D0%9A+%D0%9D%D0%90%D0%A2%D0%90%D0%9B%D0%98%D0%AF+%D0%98%D0%92%D0%90%D0%9D%D0%9E%D0%92%D0%9D%D0%90\",\"P24UJO\",\"82.207.35.20\"]"   http://127.0.0.1:8313/prolog/create/p24/new_pay_req
 
 
 #curl -d"params=[{\"name\":\"X1\"}, {\"name\":\"X2\"},{\"name\":\"Count\"} ]"    http://127.0.0.1:8313/prolog/create/user_os_c
