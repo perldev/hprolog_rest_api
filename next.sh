cff4f58580dbd3c728d96b92efefdb6eb3e0f7fe
Bogdan Tchaicka <perldev@mail.ru>
2013-06-17 13:01:44 +0300

    new api

--- next.sh
+++ next.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl     http://127.0.0.1:8313/prolog/next/p24/$1
+curl     http://127.0.0.1:8313/prolog/next/test_namespace/$1
 
 #curl -d"data={\"site\":\"http:\/\/climb.dp.ua\",\"title\":\"bogdan\",\"login\":\"none2\",\"password\":\"bezumie\",\"user_ip\":\"127.0.0.1\",\"email\":\"perldev@mail.ru\"}"    http://127.0.0.1:8086/api/registration/new
 
