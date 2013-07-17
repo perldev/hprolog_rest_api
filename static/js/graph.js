a57b8484bba75212a87a531780c63e8044c6b046
Bogdan Tchaicka <perldev@mail.ru>
2013-07-17 18:41:12 +0300

    put all code to the one namespace

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,146 +1,155 @@
-var vm_statistic = {};
+"use strict";
 
-vm_statistic.events = {};
-
-var FactName = "Init";
-
-var timerId;
-
-var page = "graph";
-
-var sideBarId;
-
-// need change host
-//vm_statistic.ws = new WebSocket("ws://hd-test-2.ceb.loc:8313/websocket");
-
- vm_statistic.ws = new WebSocket("ws://localhost:8313/websocket");
-
-vm_statistic.ws.onopen = function(evt){
-    console.log("Socket open");
-    $("#hero_unit").hide();
-    $("#monitor_menu").hide();
-    eventData();
-    vm_statistic.ws.send(JSON.stringify({action: "get", cmd: "namespaces"}));
+$(document).ready(function() {
+     $('a').on('click', vm_statistic.events.pageChange);
+     $("a[id='button']").on('click', vm_statistic.events.buttonChange);
     
-}
+});
 
-// $(document).ready(function() {
-//     chart = new Highcharts.Chart(options);
-// });
+var vm_statistic = {
+    page: "graph",
+    timerId: null,
+    sideBarId: null,  
+    options : {
+                title: 'Facts bubbles',
+                hAxis: {title: 'Popularity', maxValue: 1000000, minValue: -100000  },
+                vAxis: {title: 'Weight', maxValue: 50, minValue:-10 },
+                width:1024,
+                height:400,
+                bubble: {textStyle: {fontSize: 11}},
+                colorAxis: {colors: ['yellow', 'blue']}
+            },
+    events: 
+    {
+        buttonChange : function(evt){
+                            var state = $(this).attr('data-button');
+                            vm_statistic.ws.send(JSON.stringify({action: "change", cmd: "status", value: state}));
+                        },
+        pageChange : function(evt){
+                            vm_statistic.page = $(this).attr('data-page');
+                            vm_statistic.clear_sel_namespace();
+                            var sideBarId = vm_statistic.sideBarId;
 
-vm_statistic.ws.onclose = function(){
-    console.log("Socket closed"); 
-}
+                            switch (vm_statistic.page) {
+                                case "graph":
+                                vm_statistic.emptyInstance();
+                                clearInterval(vm_statistic.timerId);
+                                $("#monitor_menu").hide();
+                                $("#hero_unit").hide();
+                                $("#namespaces").show();
+                                $("#graph").show(); 
+                                    console.log("load graph_page");
+                                break
+                            case "monitor":
+                                vm_statistic.emptyInstance();
+                                clearInterval();
+                                $("#graph").hide();
+                                $("#namespaces").show();
+                                $("#hero_unit").show();
+                                $("#monitor_menu").show();
+                                vm_statistic.sideBarId = "requests"; 
+                                console.log("load monitor_page");
+                            break
+                            case "sidebar":
+                                vm_statistic.emptyInstance();
+                                clearInterval(vm_statistic.timerId);
+                                vm_statistic.sideBarId = $(this).attr('id');
+                                $('li').removeClass('active');
+                                $(this).parent().attr('class', 'active');
+                                vm_statistic.switchSidebars(sideBarId);
+                            }  
+                        }
+                        
 
-vm_statistic.events.pageChange = function(evt){
-    page = $(this).attr('data-page');
-    clear_sel_namespace();
-    switch (page) {
-	case "graph":
-        emptyInstance();
-        clearInterval(timerId);
-        $("#monitor_menu").hide();
-        $("#hero_unit").hide();
-        $("#namespaces").show();
-        $("#graph").show(); 
-	    console.log("load graph_page");
-	break
-    case "monitor":
-        emptyInstance();
-        clearInterval(timerId);
-        $("#graph").hide();
-        $("#namespaces").show();
-        $("#hero_unit").show();
-        $("#monitor_menu").show();
-        sideBarId = "requests"; 
-        console.log("load monitor_page");
-    break
-    case "sidebar":
-        emptyInstance();
-        clearInterval(timerId);
-        sideBarId = $(this).attr('id');
-        $('li').removeClass('active');
-        $(this).parent().attr('class', 'active');
-        switchSidebars(sideBarId);
-    }  
-}
+        
+    },
 
-vm_statistic.events.buttonChange = function(evt){
-    var state = $(this).attr('data-button');
-    vm_statistic.ws.send(JSON.stringify({action: "change", cmd: "status", value: state}));
-}
+   eventData: function  (){
+            var requests = [], namespaces = [], system_data = []; 
+            var arrey = [], GraphData = [], code_memory_data = [];
+            vm_statistic.ws.onmessage = function(evt) { 
+                        console.log("Received_msg:", evt.data);
+                        var msg = JSON.parse(evt.data);
+                        console.log("cmd:", msg.cmd);
+                        switch (msg.cmd){
+                            case "namespaces":
+                                    var namespaces = msg.namespaces;
+                                    console.log("namespaces:", namespaces);
+                                    vm_statistic.appendNamespaces(namespaces);
+                            break;
+                        case "graph":
+                            var Array = msg.graph_data;
+                            vm_statistic.drawChart(Array);
+                            vm_statistic.unblock();
+                            break;
 
-$('a').on('click', vm_statistic.events.pageChange);
-$("a[id='button']").on('click', vm_statistic.events.buttonChange);
+                        case "requests":
+                            $("#requests_container").empty();
+                            var requests = msg.requests;
+                                console.log("requests:", requests);
+                                $("#requests_container").append("<br/>");
+                            $("#requests_container").append("<div> Number Requests: " + msg.count + "</div>");
+                                $.map(requests, function(req1){
+                                $("#requests_container").append("<br/>");
+                                $("#requests_container").append("<div>" + req1.request + "</div>"); 
+                                });
+                            break;
+                        case "system_state":
+                            var system_data = msg.system_state;
+                            $("#system_state_container").empty();
+                                $("#system_state_container").append("<br/>");
+                            $("#system_state_container").append("<div> Number Processes: "+ msg.processes + " Memory/mb: " + msg.memory + "</div>");
+                                $("#system_state_container").append("<br/>");
+                            $("#system_state_container").append("<div> Number Requests: " + msg.count + "</div>");
+                            var  requests = msg.system_state;
+                                $.map(requests, function(req1){
+                                $("#system_state_container").append("<br/>");
+                                $("#system_state_container").append("<div>" + req1.state + "</div>");
+                                });
+                            break;
+                        case "code_memory":
+                            var code_memory_data = msg.code_memory;
+                            $("#code_memory_container").empty();
+                            $("#code_memory_container").append("<br/>");
+                            $("#code_memory_container").append("<div>" + code_memory_data + "</div>");
+                            break;
+                        }    
+                } 
+     },
+    clear_sel_namespace:function(){
+            $("span.btn-warning").removeClass("btn-warning").addClass("btn-info")
 
-function switchSidebars(){
-     clear_sel_namespace();
-    console.log("sideBarId:", sideBarId);
-    switch (sideBarId){
-        case "requests":
-            $("#namespaces").show();
-        break
-        case "code_memory":
-            $("#namespaces").show();
-        break
-        case "system_state":
-            $("#namespaces").hide();
-            timerId = setInterval('vm_statistic.ws.send(JSON.stringify({action: "get", cmd: sideBarId}))', 3000);
-        break   
-    }
-}
-
- 
-// var options = { chart: {
-//                 renderTo: 'graph',
-// 	        type: 'bubble',
-// 	        zoomType: 'xy',
-//                 events: { load: eventData}
-// 	    },
-//         title: {
-// 	    	text: 'Counter Facts'
-// 	    },
-//            
-// 	    legend: { enabled: false},
-// 	    series: [{
-//                     name: FactName, 
-//                     dataLabels: {
-//                             enabled: true,
-//                             x:40,
-//                             formatter:function() {
-//                                 return this.point.name;
-//                             },
-//                             style:{color:"black"}
-//                     },
-//                     data: genMassive()
-// 	}]
-// }
-var options = {
-          title: 'Facts bubbles',
-          hAxis: {title: 'Popularity', maxValue: 1000000, minValue: -100000  },
-          vAxis: {title: 'Weight', maxValue: 50, minValue:-10 },
-          width:1024,
-          height:400,
-          bubble: {textStyle: {fontSize: 11}},
-          colorAxis: {colors: ['yellow', 'blue']}
-        };
-
-
-
-// generate an array of random data to graph 1000 points
-// function genMassive()  {
-//                     var data = [],
-//                     time = (new Date()).getTime(),i;
-//                     for (i = -1000; i <= 0; i++) {
-//                         data.push({
-//                             x: Math.floor((Math.random()*100)+1),
-//                             y: Math.floor((Math.random()*100)+1),
-//                             z: Math.floor((Math.random()*100)+1),
-//                         });
-//                     }
-//                     return data;
-// }
-function drawChart(Array){
+    },
+    block: function(){
+     $("#block").show()
+    
+    },
+    unblock: function(){
+     $("#block").hide('fast')
+    
+    },
+    switchSidebars: function(){
+        var sideBarId = vm_statistic.sideBarId;
+        vm_statistic.clear_sel_namespace();
+        console.log("sideBarId:", sideBarId);
+        switch (sideBarId){
+            case "requests":
+                $("#namespaces").show();
+                break;
+            case "code_memory":
+                $("#namespaces").show();
+                break;
+            case "system_state":
+                $("#namespaces").hide();
+                
+                vm_statistic.timerId = setInterval(function(){
+                                                            vm_statistic.ws.send(JSON.stringify({action: "get", cmd: vm_statistic.sideBarId}))
+                                                        }
+                                                        , 3000);
+                break;
+        }
+    },
+    drawChart: function(Array){
 //     [
 //           ['ID', 'Popularity', 'Weight', 'Seria',     'Facts Count'],
 //           ['CAN',    80.66,              1.67,      'North America',  33739900],
@@ -160,130 +169,95 @@ function drawChart(Array){
 //     graph        
        var ResArray = [  ['ID', 'Popularity', 'Weight', 'Seria',     'Facts Count'] ];
        $.map( Array, function(Point){
-                    var NewArray = [ Point.name, Point.data[0], Point.data[1],  "Facts bubbles", Point.data[2] ];
+                    var NewArray = [ '', Point.data[0], Point.data[1],  Point.name, Point.data[2] ];
                     ResArray.push(NewArray);
        });
        var data = google.visualization.arrayToDataTable(ResArray );   
        var chart = new google.visualization.BubbleChart(document.getElementById('graph'));
-       chart.draw(data, options);      
+       chart.draw(data, this.options);      
     
 
-}
-
-
-function eventData(){
-    var requests = [], namespaces = [], system_data = []; 
-    var arrey = [], GraphData = [], code_memory_data = [];
-    vm_statistic.ws.onmessage = function(evt) { 
-	console.log("Received_msg:", evt.data);
-        var msg = JSON.parse(evt.data);
-	console.log("cmd:", msg.cmd);
-	switch (msg.cmd){
-            
-	    case "namespaces":
-		    namespaces = msg.namespaces;
-		    console.log("namespaces:", namespaces);
-		    appendNamespaces(namespaces);
-	    break
+    },
+    emptyInstance: function(){
+        $("#requests_container").empty();
+        $("#system_state_container").empty();
+        $("#code_memory_container").empty();
+    },
+    appendNamespaces :function (NameSpaces){
+            var Res = "NameSpaces : ";
+            for(var i in NameSpaces) {
+            var Name = NameSpaces[i];
+            Res +="<span id='"+ Name +"' class='btn btn-info' style='height:20px' onclick=\"vm_statistic.sendName(this)\" >" + Name + "</span>&nbsp;";
+            }  
+            $("#namespaces").html(Res);                
+     },   
+    sendName :function(obj){
+        var NameSpace = obj.id;
+        vm_statistic.clear_sel_namespace();
+        $(obj).removeClass("btn-info");
+        $(obj).addClass("btn-warning");
+        var sideBarId = vm_statistic.sideBarId;
+        var page =  vm_statistic.page;
+        switch(page){
         case "graph":
-            var Array = msg.graph_data;
-            drawChart(Array);
-            unblock();
-	    break;
+            console.log("graph_page");
+            clearInterval(vm_statistic.timerId);
+            vm_statistic.block();
+            vm_statistic.ws.send(JSON.stringify({action: "get", namespace: NameSpace, cmd: page}));
+            break;
+        case "monitor":
+                console.log("requests_page");
+                
+        case "sidebar":
+            switch (sideBarId){
+            case "requests":
+                clearInterval(vm_statistic.timerId);
+                vm_statistic.timerId = setInterval(function(){
+                        
+                        vm_statistic.ws.send(JSON.stringify({action: "get", namespace: NameSpace, cmd: sideBarId}))
+                    
+                        
+                }, 3000);
+            break;
+            case "code_memory":
+                clearInterval(vm_statistic.timerId);
+                vm_statistic.ws.send(JSON.stringify({action: "get", namespace: NameSpace, cmd: sideBarId}))
+            break;
 
-        case "requests":
-            $("#requests_container").empty();
-            requests = msg.requests;
-	        console.log("requests:", requests);
-	        $("#requests_container").append("<br/>");
-            $("#requests_container").append("<div> Number Requests: " + msg.count + "</div>");
-	        $.map(requests, function(req1){
-	        $("#requests_container").append("<br/>");
-	        $("#requests_container").append("<div>" + req1.request + "</div>"); 
-	        });
-	    break
-        case "system_state":
-            system_data = msg.system_state;
-            $("#system_state_container").empty();
-	        $("#system_state_container").append("<br/>");
-            $("#system_state_container").append("<div> Number Processes: "+ msg.processes + " Memory/mb: " + msg.memory + "</div>");
-	        $("#system_state_container").append("<br/>");
-            $("#system_state_container").append("<div> Number Requests: " + msg.count + "</div>");
-	        requests = msg.system_state;
-	        $.map(requests, function(req1){
-		    $("#system_state_container").append("<br/>");
-            $("#system_state_container").append("<div>" + req1.state + "</div>");
- 	        });
-        break
-        case "code_memory":
-            code_memory_data = msg.code_memory;
-            $("#code_memory_container").empty();
-            $("#code_memory_container").append("<br/>");
-            $("#code_memory_container").append("<div>" + code_memory_data + "</div>");
-        break
-        }    
-    } 
-}
+            }
+        } 
+    }
+           
+};
 
-function emptyInstance(){
-    $("#requests_container").empty();
-    $("#system_state_container").empty();
-    $("#code_memory_container").empty();
-}
 
-function appendNamespaces(NameSpaces){
-    Res = "NameSpaces : ";
-    for( i in NameSpaces) {
-    var Name = NameSpaces[i];
-    Res +="<span id='"+ Name +"' class='btn btn-info' style='height:20px' onclick=\"sendName(this)\" >" + Name + "</span>&nbsp;";
-     }  
-    $("#namespaces").html(Res);                
-}
 
-function sendName(obj){
-    NameSpace = obj.id;
-     clear_sel_namespace();
-    $(obj).removeClass("btn-info");
-    $(obj).addClass("btn-warning");
 
-    
-    
-    switch(page){
-    case "graph":
-        console.log("graph_page");
-    	clearInterval(timerId);
-        block();
-    	timerId = setInterval('vm_statistic.ws.send(JSON.stringify({action: "get", namespace: NameSpace, cmd: page}))', 30000);
-    break
-    case "monitor":
-	    console.log("requests_page");
-    case "sidebar":
-        switch (sideBarId){
-        case "requests":
-            clearInterval(timerId);
-            timerId = setInterval('vm_statistic.ws.send(JSON.stringify({action: "get", namespace: NameSpace, cmd: sideBarId}))', 3000);
-        break
-        case "code_memory":
-            clearInterval(timerId);
-            vm_statistic.ws.send(JSON.stringify({action: "get", namespace: NameSpace, cmd: sideBarId}))
-        break
+// need change host
+//vm_statistic.ws = new WebSocket("ws://hd-test-2.ceb.loc:8313/websocket");
 
-        }
-    } 
-}
-function clear_sel_namespace(){
-    
-        $("span.btn-warning").removeClass("btn-warning").addClass("btn-info")
-
-}
-function block(){
-     $("#block").show()
+vm_statistic.ws = new WebSocket("ws://localhost:8313/websocket");
+vm_statistic.ws.onopen = function(evt){
+    console.log("Socket open");
+    $("#hero_unit").hide();
+    $("#monitor_menu").hide();
+    vm_statistic.eventData();
+    vm_statistic.ws.send(JSON.stringify({action: "get", cmd: "namespaces"}));
     
 }
-function unblock(){
-     $("#block").hide('fast')
-    
+vm_statistic.ws.onclose = function(){
+    console.log("Socket closed"); 
 }
+
+
+
+
+ 
+
+
+
+
+
 
 
 
