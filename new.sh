508d49742a59267746e36c3ff1fe6da88328c04c
Bogdan Tchaicka <perldev@mail.ru>
2013-08-15 16:27:15 +0300

    adding rebar

--- new.sh
+++ new.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
      
-# curl -d "params=[\"P24test4477854708018\",\"TDNHE\",\"13.1\",\"UAH\",\"first test sender account\",\"4405882311725711\",\"0716\",\"+380663060686\",\"first receiver account name\",\"5211537329947530\",\"0716\",\"380663060686\",\"380269\",\"2807903329\",\"test details\",\"159.224.129.106\" ]"   http://127.0.0.1:8313/prolog/create/p24/new_pay_req
+curl -d "params=[\"_P24test4477854708018\",\"TDNHE\",\"13.1\",\"UAH\",\"first test sender account\",\"4405882311725711\",\"0716\",\"+380663060686\",\"first receiver account name\",\"5211537329947530\",\"0716\",\"380663060686\",\"380269\",\"2807903329\",\"test details\",\"159.224.129.106\" ]"   http://127.0.0.1:8313/prolog/create/p24/new_pay_req
 
 
 
-curl -d"params=[{\"name\":\"X1\"} ]"    http://127.0.0.1:8313/prolog/create/test/pay
+ #curl -d"params=[{\"name\":\"X1\"} ]"    http://127.0.0.1:8313/prolog/create/test/pay
 
 #curl -d"data={\"site\":\"http:\/\/climb.dp.ua\",\"title\":\"bogdan\",\"login\":\"none2\",\"password\":\"bezumie\",\"user_ip\":\"127.0.0.1\",\"email\":\"perldev@mail.ru\"}"    http://127.0.0.1:8086/api/registration/new
 
--- src/prolog_open_api_sup.erl
+++ src/prolog_open_api_sup.erl
@@ -2,8 +2,13 @@
 -behaviour(supervisor).  
 -export([start_link/0]).  
 -export([init/1]).  
--include("open_api.hrl").  
-  
+-include("open_api.hrl").
+-include("deps/eprolog/include/prolog.hrl").
+%   'X10' = 29247833500110 'X13' = 14360570 'X3' = 7.0 'X16' = 37.46.235.30
+%   'X6' = 4627085826005098 'Ref' = P24A133251880884926 'X9' = Днепрогаз ПАО
+%   'X12' = 305299 'X2' = DNHE 'X15' = P24BPL 'X5' = Чумаченко Виктория Антоновна 
+%   'X18' = nothing 'X8' = +380679779890 'X11' = nothing 
+%   'X14' = Днепрогаз ПАО 'X4' = UAH 'X17' = 35614337 'X7' = 0614 Yes looking next ?
 start_link() ->  
         supervisor:start_link({local, ?MODULE}, ?MODULE, []).  
       
@@ -16,8 +21,15 @@ init([]) ->
         {converter_monitor, start_link, [ LogFunction ]},
         permanent, infinity, worker , [ converter_monitor]
         },
-        AuthDemon = {"auth_demon",
+        AuthDemon = {
+        "auth_demon",
             {auth_demon, start_link, [] },
             permanent, infinity, worker , [ auth_demon]
+        
         },
-        {ok, {{one_for_one, 5, 10}, [Restarter, AuthDemon]}}.  
+        
+        ThriftPool = {"thrift_connection_pool",
+            {thrift_connection_pool, start_link, [ ?DEFAULT_COUNT_THRIFT ] },
+            permanent, infinity, worker , [ thrift_connection_pool ]
+        },
+        {ok, {{one_for_one, 5, 10}, [Restarter, AuthDemon, ThriftPool]}}.  
